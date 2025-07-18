8145127880e3236c565fa28207e5fb74b2b430c0
qr212345 <asahi114711@icloud.com>
2025-07-18 19:13:38 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /**********************
  *  ババ抜き大会管理  *
  **********************/
-import { Html5Qrcode } from "https://unpkg.com/html5-qrcode@2.3.7/html5-qrcode.min.js";
 const SCAN_COOLDOWN_MS = 1500;
 const POLL_INTERVAL_MS = 20_000;
 
